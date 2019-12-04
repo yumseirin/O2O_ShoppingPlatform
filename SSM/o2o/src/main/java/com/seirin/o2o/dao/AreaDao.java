c485e2e604aeb247b23d01663b083bf43b5fe4fd
seirin <43994152+yumseirin@users.noreply.github.com>
2019-12-04 16:32:11 +0800

    Create AreaDao.java
    创建AreaDao接口

---
+++ SSM/o2o/src/main/java/com/seirin/o2o/dao/AreaDao.java
@@ -0,0 +1,15 @@
+package com.seirin.o2o.dao;
+
+import java.util.List;
+
+import com.seirin.o2o.entity.Area;
+
+public interface AreaDao {
+
+	/**
+	 * 列出区域列表
+	 * 
+	 * @return areaList
+	 */
+	List<Area> queryArea();
+}
