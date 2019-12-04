eecb5658868a168c01a38c1b9794991920f9c926
seirin <43994152+yumseirin@users.noreply.github.com>
2019-12-04 11:12:21 +0800

    Create ProductCategory.java
    创建ProductCategory商品类别实体类

---
+++ SSM/o2o/src/main/java/com/seirin/o2o/entity/ProductCategory.java
@@ -0,0 +1,84 @@
+package com.seirin.o2o.entity;
+
+import java.util.Date;
+
+/**
+ * 商品类别
+ * 
+ * @author seirin
+ *
+ */
+public class ProductCategory {
+
+	// 商品类别ID
+	private Long productCategoryId;
+	// 商铺ID
+	private Long shopId;
+	// 名称
+	private String productCategoryName;
+	// 描述
+	private String productCategoryDesc;
+	// 权重
+	private Integer priority;
+	// 创建时间
+	private Date createTime;
+	// 修改时间
+	private Date lastEditTime;
+
+	public Long getProductCategoryId() {
+		return productCategoryId;
+	}
+
+	public void setProductCategoryId(Long productCategoryId) {
+		this.productCategoryId = productCategoryId;
+	}
+
+	public Long getShopId() {
+		return shopId;
+	}
+
+	public void setShopId(Long shopId) {
+		this.shopId = shopId;
+	}
+
+	public String getProductCategoryName() {
+		return productCategoryName;
+	}
+
+	public void setProductCategoryName(String productCategoryName) {
+		this.productCategoryName = productCategoryName;
+	}
+
+	public String getProductCategoryDesc() {
+		return productCategoryDesc;
+	}
+
+	public void setProductCategoryDesc(String productCategoryDesc) {
+		this.productCategoryDesc = productCategoryDesc;
+	}
+
+	public Integer getPriority() {
+		return priority;
+	}
+
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getLastEditTime() {
+		return lastEditTime;
+	}
+
+	public void setLastEditTime(Date lastEditTime) {
+		this.lastEditTime = lastEditTime;
+	}
+
+}
